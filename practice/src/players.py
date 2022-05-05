457c055e096bd8c5285718dccaeafc648dbb839a
takaya0 <ytk.koizumi@gmail.com>
2022-05-06 00:02:09 +0900

    add reversi_game.py

---
+++ practice/src/players.py
@@ -0,0 +1,70 @@
+import torch
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+k = 192
+fcl_units = 256
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class DQN(nn.Module):
+
+    def __init__(self):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(2, k, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(k)
+        self.conv2 = nn.Conv2d(k, k, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(k)
+        self.conv3 = nn.Conv2d(k, k, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(k)
+        self.conv4 = nn.Conv2d(k, k, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(k)
+        self.conv5 = nn.Conv2d(k, k, kernel_size=3, padding=1)
+        self.bn5 = nn.BatchNorm2d(k)
+        self.conv6 = nn.Conv2d(k, k, kernel_size=3, padding=1)
+        self.bn6 = nn.BatchNorm2d(k)
+        self.conv7 = nn.Conv2d(k, k, kernel_size=3, padding=1)
+        self.bn7 = nn.BatchNorm2d(k)
+        self.conv8 = nn.Conv2d(k, k, kernel_size=3, padding=1)
+        self.bn8 = nn.BatchNorm2d(k)
+        self.conv9 = nn.Conv2d(k, k, kernel_size=3, padding=1)
+        self.bn9 = nn.BatchNorm2d(k)
+        self.conv10 = nn.Conv2d(k, k, kernel_size=3, padding=1)
+        self.bn10 = nn.BatchNorm2d(k)
+        self.fcl1 = nn.Linear(k * 64, fcl_units)
+        self.fcl2 = nn.Linear(fcl_units, fcl_units)
+        self.fcl3 = nn.Linear(fcl_units, 65)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = F.relu(self.bn6(self.conv6(x)))
+        x = F.relu(self.bn7(self.conv7(x)))
+        x = F.relu(self.bn8(self.conv8(x)))
+        x = F.relu(self.bn9(self.conv9(x)))
+        x = F.relu(self.bn10(self.conv10(x)))
+        x = F.relu(self.fcl1(x.view(-1, k * 64)))
+        x = F.relu(self.fcl2(x))
+        x = self.fcl3(x)
+        return x.tanh()
+
+
+class DQN_Player():
+    def __init__(self, model_file):
+        self.model = DQN().to(device)
+        self.model.load_state_dict(torch.load(
+            model_file)['state_dict'])
+
+    def select_place(self, state, legal_places):
+        with torch.no_grad():
+            next_actions = torch.tensor(
+                [legal_places], device=device, dtype=torch.long)
+            q = self.model(state)
+            legal_q = q.gather(1, next_actions)
+            return legal_places[legal_q.argmax(dim=1).item()]
